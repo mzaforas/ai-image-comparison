a415fd013214c79bf221a48404fc701d8054a3ea
Manuel Zaforas <mzaforas@paradigmadigital.com>
2017-09-19 11:40:44 +0200

    Playing with google api

--- examples/vision.py
+++ examples/vision.py
@@ -4,7 +4,8 @@ import pprint
 client = vision.Client()
 
 BUCKET = 'iniciativa-big-data'
-IMAGE_NAME = 'examples/tyrion.jpg'
+# IMAGE_NAME = 'examples/tyrion.jpg'
+IMAGE_NAME = '/home/mzaforas/Descargas/IMG_20170905_160110.jpg'
 
 # Using Storage
 # image = client.image(source_uri='gs://{}/{}'.format(BUCKET, IMAGE_NAME))
@@ -13,26 +14,39 @@ IMAGE_NAME = 'examples/tyrion.jpg'
 with open(IMAGE_NAME, 'rb') as image_file:
     image = client.image(content=image_file.read())
 
-# Detect faces
-response = image.detect_faces()
-for face in response:
-    pprint.pprint({
-        'roll_angle': face.angles.roll,
-        'pan_angle': face.angles.pan,
-        'tilt_angle': face.angles.tilt,
-        'detection_confidence': face.detection_confidence,
-        'landmarking_confidence': face.landmarking_confidence,
-        'joy': face.joy,
-        'sorrow': face.sorrow,
-        'anger': face.anger,
-        'surprise': face.surprise,
-        'under_exposed': face.image_properties.underexposed,
-        'blurred': face.image_properties.blurred,
-        'headwear': face.headwear,
-    })
+# # Detect faces
+# response = image.detect_faces()
+# for face in response:
+#     pprint.pprint({
+#         'roll_angle': face.angles.roll,
+#         'pan_angle': face.angles.pan,
+#         'tilt_angle': face.angles.tilt,
+#         'detection_confidence': face.detection_confidence,
+#         'landmarking_confidence': face.landmarking_confidence,
+#         'joy': face.joy,
+#         'sorrow': face.sorrow,
+#         'anger': face.anger,
+#         'surprise': face.surprise,
+#         'under_exposed': face.image_properties.underexposed,
+#         'blurred': face.image_properties.blurred,
+#         'headwear': face.headwear,
+#     })
+#
+#
+# # Detect labels
+# response = image.detect_labels()
+# for label in response:
+#     print(label.description, label.score)
 
-
-# Detect labels
-response = image.detect_labels()
-for label in response:
-    print(label.description, label.score)
+response = image.detect_full_text()
+pprint.pprint(response.text)
+# for e in response:
+#     pprint.pprint((e.bounds, e.description, e.score))
+# bounds',
+#  'description',
+#  'from_api_repr',
+#  'from_pb',
+#  'locale',
+#  'locations',
+#  'mid',
+#  'score']
